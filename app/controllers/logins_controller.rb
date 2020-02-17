fe46f2cdc7651b02e48424d62483447203e025d7
Andrew Luchuk <spaxumof@gmail.com>
2020-02-17 10:59:21 -0500

    adds functionality to logins controller

--- app/controllers/logins_controller.rb
+++ app/controllers/logins_controller.rb
@@ -1,2 +1,26 @@
 class LoginsController < ApplicationController
+
+  def welcome
+    @client_id = ENV['CLIENT_ID']
+  end
+
+  def callback
+    puts params
+    client = Octokit::Client.new :client_id => ENV['CLIENT_ID'], :client_secret => ENV['CLIENT_SECRET']
+    access_code = client.exchange_code_for_token(params[:code])
+    session['token'] = access_code
+    redirect_to repos_path
+  end
+
+  def repos
+    access_code = session['token']
+    puts "------------------"
+    puts access_code[0][1]
+    client = Octokit::Client.new :access_token => access_code[0][1]
+    puts "################################"
+    @user = client.user
+    puts @user.name
+    @repos = client.repos
+    puts @repos
+  end
 end
